94b47db20a672e2a4da40a32cb2e81f5c8a7e242
km-v1 <308789073+kristinavirtualassistant-coder@users.noreply.github.com>
2026-09-24 01:39:11 +0800

    Configure container runtime database environment

--- deploy/cloudflare-app/worker.ts
+++ deploy/cloudflare-app/worker.ts
@@ -1,9 +1,19 @@
 import { Container, getContainer } from '@cloudflare/containers';
+import { env } from 'cloudflare:workers';
 
 export class VortexOneContainer extends Container {
   defaultPort = 8080;
   sleepAfter = '10m';
   enableInternet = true;
+  envVars = {
+    SQL_HOST: env.SQL_HOST,
+    SQL_PORT: env.SQL_PORT,
+    SQL_DB_NAME: env.SQL_DB_NAME,
+    SQL_USER: env.SQL_USER,
+    SQL_PASSWORD: env.SQL_PASSWORD,
+    SQL_SSL: env.SQL_SSL,
+    VORTEX_ONE_SKIP_MIGRATIONS: env.VORTEX_ONE_SKIP_MIGRATIONS,
+  };
 }
 
 type Env = {
